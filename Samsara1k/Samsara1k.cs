cdac9c9d7ee7caf1bc036b0ad652472af87f08a8
sth4nothing <892223922@qq.com>
2018-10-17 14:02:59 +0800

    1.0.1-自定义增加的轮回数

--- Samsara1k/Samsara1k.cs
+++ Samsara1k/Samsara1k.cs
@@ -15,7 +15,7 @@ namespace Samsara1k
         {
             if (Main.enabled)
             {
-                DateFile.instance.samsara += 999;
+                DateFile.instance.samsara += Main.settings.samsara - 1;
             }
         }
     }
@@ -29,7 +29,7 @@ namespace Samsara1k
             {
                 if (MassageWindow.instance.eventValue[1] == 1)
                 {
-                    DateFile.instance.samsara += 999;
+                    DateFile.instance.samsara += Main.settings.samsara - 1;
                 }
             }
         }
@@ -41,6 +41,8 @@ namespace Samsara1k
 
         public static bool enabled;
 
+        public static Settings settings;
+
         public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
         {
             enabled = value;
@@ -50,9 +52,41 @@ namespace Samsara1k
         public static bool Load(UnityModManager.ModEntry modEntry)
         {
             logger = modEntry.Logger;
+            settings = Settings.Load<Settings>(modEntry);
             modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(OnToggle);
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
             HarmonyInstance.Create(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
             return true;
         }
+        
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("每次传剑增加轮回次数:", GUILayout.Width(180));
+            int samsara;
+            if (int.TryParse(GUILayout.TextArea(settings.samsara.ToString(), GUILayout.Width(60)), out samsara))
+            {
+                if (samsara >= 1)
+                {
+                    settings.samsara = samsara;
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        public static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+    }
+
+    public class Settings : UnityModManager.ModSettings
+    {
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+        }
+        public int samsara = 100;
     }
 }
