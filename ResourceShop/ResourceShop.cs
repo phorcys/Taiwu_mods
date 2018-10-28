6417ea233a50e24b728c245c8a9ffe36f1819680
xyzkljl1 <xyzkljl1@yahoo.com>
2018-10-28 19:09:47 +0800

    ADD Mod ResourceShop v1.0.0： Buy Resource with gold

---
+++ ResourceShop/ResourceShop.cs
@@ -0,0 +1,92 @@
+﻿using Harmony12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityModManagerNet;
+
+namespace ResourceShop
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+        }
+        public int resourceType = 0;
+        public int resourceCount = 0;
+    }
+
+    public static class Main
+    {
+        public static bool enabled;
+        public static Settings settings;
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static String[] toolBarText = { "食材", "木材", "金石", "织物", "药材" };
+        public static float price=1.5f;
+        //金钱在资源中的Index
+        public const int MoneyIndex = 5;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            settings = Settings.Load<Settings>(modEntry);
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(string.Format("以{0}价格购买",price), GUILayout.Width(90));
+            Main.settings.resourceType = GUILayout.Toolbar(Main.settings.resourceType, toolBarText);
+            int.TryParse(GUILayout.TextField(Main.settings.resourceCount.ToString(), 5, GUILayout.Width(90)), out Main.settings.resourceCount);
+            DateFile tbl = DateFile.instance;
+            if (tbl == null || tbl.actorsDate == null || !tbl.actorsDate.ContainsKey(tbl.mianActorId))
+            {
+                GUILayout.Label("  存档未载入!");
+            }
+            else
+            {
+                if (GUILayout.Button("确认购买"))
+                    BuyResource();
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+        private static void BuyResource()
+        {
+            if (!Main.enabled)
+                return;
+            int id = DateFile.instance.MianActorID();
+            int count = Main.settings.resourceCount;
+            int cost =(int) Math.Ceiling(price * (float)count);
+            int current_money=ActorMenu.instance.ActorResource(id)[MoneyIndex];
+            if(cost>current_money)
+            {
+                cost = current_money;
+                count =(int) Math.Floor((float)cost / price);
+            }
+            UIDate.instance.ChangeResource(id, Main.settings.resourceType, count, true);
+            UIDate.instance.ChangeResource(id, MoneyIndex, -count, true);
+            Main.settings.resourceCount = 0;
+        }
+    }
+
+}
