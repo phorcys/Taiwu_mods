438ab2fb1095ee448ba5daf7605872197f70e11e
ignaz chou <zhpjie@gmail.com>
2018-10-31 17:28:39 +0800

    修正了部分冗余代码，降低耦合

--- MoreInfo/MoreInfo.cs
+++ MoreInfo/MoreInfo.cs
@@ -22,7 +22,11 @@ namespace MoreInfo
         public bool showItemExtraInBag = true;//显示包裹中物品特殊词条
         public bool showItemExtraInBank = true;//显示仓库中物品特殊词条
         public bool showItemExtraInShop = true;//显示商店中物品特殊词条
+        public bool showGongFaLevel = true;//显示功法等级颜色
+
     }
+
+
 
     public static class Main
     {
@@ -54,9 +58,45 @@ namespace MoreInfo
             Main.settings.showItemExtraInBag = GUILayout.Toggle(Main.settings.showItemExtraInBag, "显示包裹中物品特殊词条", new GUILayoutOption[0]);
             Main.settings.showItemExtraInBank = GUILayout.Toggle(Main.settings.showItemExtraInBank, "显示仓库中物品特殊词条", new GUILayoutOption[0]);
             Main.settings.showItemExtraInShop = GUILayout.Toggle(Main.settings.showItemExtraInShop, "显示商店中物品特殊词条", new GUILayoutOption[0]);
+            Main.settings.showGongFaLevel = GUILayout.Toggle(Main.settings.showGongFaLevel, "显示功法等级颜色", new GUILayoutOption[0]);
 
             GUILayout.EndHorizontal();
         }
+
+        public static Dictionary<int, string> itemExtraAttr = new Dictionary<int, string>()
+               {
+                    { 50501,"音律"},
+                    { 50502,"弈棋"},
+                    { 50503,"诗书"},
+                    { 50504,"绘画"},
+                    { 50505,"术数"},
+                    { 50506,"品鉴"},
+                    { 50507,"锻造"},
+                    { 50508,"制木"},
+                    { 50509,"医术"},
+                    { 50510,"毒术"},
+                    { 50511,"织锦"},
+                    { 50512,"巧匠"},
+                    { 50513,"道法"},
+                    { 50514,"佛学"},
+                    { 50515,"厨艺"},
+                    { 50516,"杂学"},
+
+                    { 50601,"内功"},
+                    { 50602,"身法"},
+                    { 50603,"绝技"},
+                    { 50604,"拳掌"},
+                    { 50605,"指法"},
+                    { 50606,"腿法"},
+                    { 50607,"暗器"},
+                    { 50608,"剑法"},
+                    { 50609,"刀法"},
+                    { 50610,"长兵"},
+                    { 50611,"奇门"},
+                    { 50612,"软兵"},
+                    { 50613,"御射"},
+                    { 50614,"乐器"},
+               };
 
         static void OnSaveGUI(UnityModManager.ModEntry modEntry)
         {
@@ -75,41 +115,7 @@ namespace MoreInfo
     //将人物包裹中的的宝物耐久度替换为特殊词条显示
     [HarmonyPatch(typeof(SetItem), "SetActorMenuItemIcon")]
     public static class SetItem_SetActorMenuItemIcon_Patch
-    {               
-        public static Dictionary<int, string> itemExtraAttr = new Dictionary<int, string>()
-               {
-                    { 50501,"音律"},
-                    { 50502,"弈棋"},
-                    { 50503,"诗书"},
-                    { 50504,"绘画"},
-                    { 50505,"术数"},
-                    { 50506,"品鉴"},
-                    { 50507,"锻造"},
-                    { 50508,"制木"},
-                    { 50509,"医术"},
-                    { 50510,"毒术"},
-                    { 50511,"织锦"},
-                    { 50512,"巧匠"},
-                    { 50513,"道法"},
-                    { 50514,"佛学"},
-                    { 50515,"厨艺"},
-                    { 50516,"杂学"},
-
-                    { 50601,"内功"},
-                    { 50602,"身法"},
-                    { 50603,"绝技"},
-                    { 50604,"拳掌"},
-                    { 50605,"指法"},
-                    { 50606,"腿法"},
-                    { 50607,"暗器"},
-                    { 50608,"剑法"},
-                    { 50609,"刀法"},
-                    { 50610,"长兵"},
-                    { 50611,"奇门"},
-                    { 50612,"软兵"},
-                    { 50613,"御射"},
-                    { 50614,"乐器"},
-               };
+    {              
         static void Postfix(SetItem __instance, int actorId, int itemId, int actorFavor, int injuryTyp)
         {
              if (!Main.enabled || !Main.settings.showItemExtraInBag)
@@ -118,7 +124,7 @@ namespace MoreInfo
             //只针对宝物的属性处理
             if ( typ == 3 )
             {
-                foreach (var item in itemExtraAttr)
+                foreach (var item in Main.itemExtraAttr)
                 {
                     int val = int.Parse(DateFile.instance.GetItemDate(itemId, item.Key, false));
                     if (val > 0)
@@ -136,40 +142,6 @@ namespace MoreInfo
     [HarmonyPatch(typeof(SetItem), "SetWarehouseItemIcon")]    
     public static class SetItem_SetWarehouseItemIcon_Patch
     {
-        public static Dictionary<int, string> itemExtraAttr = new Dictionary<int, string>()
-               {
-                    { 50501,"音律"},
-                    { 50502,"弈棋"},
-                    { 50503,"诗书"},
-                    { 50504,"绘画"},
-                    { 50505,"术数"},
-                    { 50506,"品鉴"},
-                    { 50507,"锻造"},
-                    { 50508,"制木"},
-                    { 50509,"医术"},
-                    { 50510,"毒术"},
-                    { 50511,"织锦"},
-                    { 50512,"巧匠"},
-                    { 50513,"道法"},
-                    { 50514,"佛学"},
-                    { 50515,"厨艺"},
-                    { 50516,"杂学"},
-
-                    { 50601,"内功"},
-                    { 50602,"身法"},
-                    { 50603,"绝技"},
-                    { 50604,"拳掌"},
-                    { 50605,"指法"},
-                    { 50606,"腿法"},
-                    { 50607,"暗器"},
-                    { 50608,"剑法"},
-                    { 50609,"刀法"},
-                    { 50610,"长兵"},
-                    { 50611,"奇门"},
-                    { 50612,"软兵"},
-                    { 50613,"御射"},
-                    { 50614,"乐器"},
-               };
         static void Postfix(SetItem __instance, int actorId, int itemId, bool cantTake, Image itemDragDes = null, int itemDragTyp = -1)
         {
 
@@ -185,7 +157,7 @@ namespace MoreInfo
             //只针对宝物的属性处理
             if (typ == 3)
             {
-                foreach (var item in itemExtraAttr)
+                foreach (var item in Main.itemExtraAttr)
                 {
                     int val = int.Parse(DateFile.instance.GetItemDate(itemId, item.Key, false));
                     if (val > 0)
@@ -202,41 +174,7 @@ namespace MoreInfo
     //将商店中的的宝物耐久度替换为特殊词条显示
     [HarmonyPatch(typeof(SetItem), "SetShopItemIcon")]
     public static class SetItem_SetShopItemIcon_Patch
-    {
-        public static Dictionary<int, string> itemExtraAttr = new Dictionary<int, string>()
-               {
-                    { 50501,"音律"},
-                    { 50502,"弈棋"},
-                    { 50503,"诗书"},
-                    { 50504,"绘画"},
-                    { 50505,"术数"},
-                    { 50506,"品鉴"},
-                    { 50507,"锻造"},
-                    { 50508,"制木"},
-                    { 50509,"医术"},
-                    { 50510,"毒术"},
-                    { 50511,"织锦"},
-                    { 50512,"巧匠"},
-                    { 50513,"道法"},
-                    { 50514,"佛学"},
-                    { 50515,"厨艺"},
-                    { 50516,"杂学"},
-
-                    { 50601,"内功"},
-                    { 50602,"身法"},
-                    { 50603,"绝技"},
-                    { 50604,"拳掌"},
-                    { 50605,"指法"},
-                    { 50606,"腿法"},
-                    { 50607,"暗器"},
-                    { 50608,"剑法"},
-                    { 50609,"刀法"},
-                    { 50610,"长兵"},
-                    { 50611,"奇门"},
-                    { 50612,"软兵"},
-                    { 50613,"御射"},
-                    { 50614,"乐器"},
-               };
+    {       
         static void Postfix(SetItem __instance, int itemId, int itemSize, int desIndex, int changeTyp, int itemMoney, Image dragDes)
         {
             if (!Main.enabled || !Main.settings.showItemExtraInShop)
@@ -245,7 +183,7 @@ namespace MoreInfo
             //只针对宝物的属性处理
             if (typ == 3)
             {
-                foreach (var item in itemExtraAttr)
+                foreach (var item in Main.itemExtraAttr)
                 {
                     int val = int.Parse(DateFile.instance.GetItemDate(itemId, item.Key, false));
                     if (val > 0)
