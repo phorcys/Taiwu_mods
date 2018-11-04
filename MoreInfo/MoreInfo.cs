0be2b0ddd6d3b0308fd6762856d1fd3c38aec0b8
ignaz chou <zhpjie@gmail.com>
2018-11-04 09:39:14 +0800

    更新1.3.0 重构了代码结构 添加了图纸与功法书增强显示

--- MoreInfo/MoreInfo.cs
+++ MoreInfo/MoreInfo.cs
@@ -22,10 +22,14 @@ namespace MoreInfo
 
         public bool isLoaded = false;//是否为首次Loading
 
-        public bool showItemExtraInBag = true;//显示包裹中物品特殊词条
-        public bool showItemExtraInEquuipBag = true;//显示装备界面包裹中物品特殊词条
-        public bool showItemExtraInBank = true;//显示仓库中物品特殊词条
-        public bool showItemExtraInShop = true;//显示商店中物品特殊词条
+        public bool showExtraName = true;//显示特殊词条
+        public bool showBookGang = true;//显示书籍门派
+        public bool showBlueprintName = true;//显示图纸名称
+
+        public bool showInBag = true;//显示包裹中物品特殊词条
+        public bool showInEquuipBag = true;//显示装备界面包裹中物品特殊词条
+        public bool showInBank = true;//显示仓库中物品特殊词条
+        public bool showInShop = true;//显示商店中物品特殊词条
         public bool showExtraValue = false;//显示特殊词条加成值
 
 
@@ -36,13 +40,7 @@ namespace MoreInfo
         public bool showInLevelUpWindow = true;//在突破界面显示
 
         public bool showStroyLevel = true;//显示奇遇等级
-
-
-
-
     }
-
-
 
     public static class Main
     {
@@ -71,15 +69,25 @@ namespace MoreInfo
         static void OnGUI(UnityModManager.ModEntry modEntry)
         {
 
+
             GUILayout.BeginHorizontal();
-            GUILayout.Label("物品词条");
+            GUILayout.Label("显示功能");
             GUILayout.EndHorizontal();
             GUILayout.BeginHorizontal();
-            Main.settings.showItemExtraInBag = GUILayout.Toggle(Main.settings.showItemExtraInBag, "显示包裹中物品特殊词条", new GUILayoutOption[0]);
-            Main.settings.showItemExtraInEquuipBag = GUILayout.Toggle(Main.settings.showItemExtraInEquuipBag, "显示装备界面包裹中物品特殊词条", new GUILayoutOption[0]);
-            Main.settings.showItemExtraInBank = GUILayout.Toggle(Main.settings.showItemExtraInBank, "显示仓库中物品特殊词条", new GUILayoutOption[0]);
-            Main.settings.showItemExtraInShop = GUILayout.Toggle(Main.settings.showItemExtraInShop, "显示商店中物品特殊词条", new GUILayoutOption[0]);
-            Main.settings.showExtraValue = GUILayout.Toggle(Main.settings.showExtraValue, "显示加成数值", new GUILayoutOption[0]);
+            Main.settings.showExtraName = GUILayout.Toggle(Main.settings.showExtraName, "显示宝物特殊词条", new GUILayoutOption[0]);
+            Main.settings.showBookGang = GUILayout.Toggle(Main.settings.showBookGang, "显示书籍门派", new GUILayoutOption[0]);
+            Main.settings.showBlueprintName = GUILayout.Toggle(Main.settings.showBlueprintName, "显示图纸名称", new GUILayoutOption[0]);
+
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("显示区域");
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            Main.settings.showInBag = GUILayout.Toggle(Main.settings.showInBag, "包裹中显示", new GUILayoutOption[0]);
+            Main.settings.showInEquuipBag = GUILayout.Toggle(Main.settings.showInEquuipBag, "装备界面包裹显示", new GUILayoutOption[0]);
+            Main.settings.showInBank = GUILayout.Toggle(Main.settings.showInBank, "仓库中显示", new GUILayoutOption[0]);
+            Main.settings.showInShop = GUILayout.Toggle(Main.settings.showInShop, "商店中显示", new GUILayoutOption[0]);
+            //Main.settings.showExtraValue = GUILayout.Toggle(Main.settings.showExtraValue, "显示加成数值", new GUILayoutOption[0]);
 
             GUILayout.EndHorizontal();
             GUILayout.BeginHorizontal();
@@ -100,40 +108,6 @@ namespace MoreInfo
             GUILayout.EndHorizontal();
         }
 
-        public static Dictionary<int, string> itemExtraAttr = new Dictionary<int, string>()
-               {
-                    { 50501,"音律"},
-                    { 50502,"弈棋"},
-                    { 50503,"诗书"},
-                    { 50504,"绘画"},
-                    { 50505,"术数"},
-                    { 50506,"品鉴"},
-                    { 50507,"锻造"},
-                    { 50508,"制木"},
-                    { 50509,"医术"},
-                    { 50510,"毒术"},
-                    { 50511,"织锦"},
-                    { 50512,"巧匠"},
-                    { 50513,"道法"},
-                    { 50514,"佛学"},
-                    { 50515,"厨艺"},
-                    { 50516,"杂学"},
-
-                    { 50601,"内功"},
-                    { 50602,"身法"},
-                    { 50603,"绝技"},
-                    { 50604,"拳掌"},
-                    { 50605,"指法"},
-                    { 50606,"腿法"},
-                    { 50607,"暗器"},
-                    { 50608,"剑法"},
-                    { 50609,"刀法"},
-                    { 50610,"长兵"},
-                    { 50611,"奇门"},
-                    { 50612,"软兵"},
-                    { 50613,"御射"},
-                    { 50614,"乐器"},
-               };
 
         static void OnSaveGUI(UnityModManager.ModEntry modEntry)
         {
@@ -187,21 +161,62 @@ namespace MoreInfo
             return DateFile.instance.GetActorName(actorId, true, false);
         }
 
-        //获取物品类型
+        //获取功法所属门派Id
+        public int getGangId(int gongFaId)
+        {
+            return int.Parse(DateFile.instance.gongFaDate[gongFaId][3]);
+        }
+        //获取功法所属门派名称
+        public string getGangName(int gongFaId)
+        {
+            int gangId = getGangId(gongFaId);
+            return DateFile.instance.presetGangDate[gangId][0];
+        }
+
+        //获取书籍对应的功法ID
+        public int getGongFaId(int itemId)
+        {
+            int bookTyp = int.Parse(DateFile.instance.GetItemDate(itemId, 999, false));
+            //500000以上为普通秘籍，700000以上为手抄
+            return isOriginalBook(itemId) ? (bookTyp - 500000) : (bookTyp - 700000);
+        }
+
+        //判断秘籍是真传还是手抄
+        public bool isOriginalBook(int itemId)
+        {
+            //序列35 0为普通,1为手抄本
+            return int.Parse(DateFile.instance.GetItemDate(itemId, 35, false)) == 0;
+        }
+
+        //获取物品大类
         public int getItemType(int itemId)
         {
-            return int.Parse(DateFile.instance.GetItemDate(itemId, 1, false));
+            return int.Parse(DateFile.instance.GetItemDate(itemId, 4, false));
         }
+
+        //获取物品小类
+        public int getItemSecondType(int itemId)
+        {
+            return int.Parse(DateFile.instance.GetItemDate(itemId, 5, false));
+        }
+
+        //获取物品细类
+        public int getItemThirdType(int itemId)
+        {
+            return int.Parse(DateFile.instance.GetItemDate(itemId, 506, false));
+        }
+
         //获取物品名称
         public string getItemName(int itemId)
         {
             return DateFile.instance.GetItemDate(itemId, 0, false);
         }
+
         //获取特殊词条的名称
         public string getItemExtraName(int itemId)
         {
             string itemName = this.getItemName(itemId);
-            
+
             string value = "";
             foreach (var item in itemExtraAttr)
             {
@@ -209,7 +224,6 @@ namespace MoreInfo
                 if (val > 0)
                 {
                     //使用/n换行后无法显示耐久，直接接属性名后方则耐久显示不全，暂时只显示属性
-                    //__instance.itemNumber.text = !Main.settings.showExtraValue ? item.Value : item.Value + val;
                     value = item.Value;
                     break;
                 }
@@ -218,32 +232,87 @@ namespace MoreInfo
         }
 
         //变更文本内容
-        public void changeText(Text text,string newText)
+        public void changeText(Text text, string newText)
         {
             text.text = newText;
         }
+
         //变更宝物名称
-        public void changeDecName(Text text,int itemId)
+        public void changeDecName(Text text, int itemId)
         {
-            int typ = this.getItemType(itemId);
-            if (typ != 3) return;
             string extraName = getItemExtraName(itemId);
             if (extraName != "")
             {
                 changeText(text, extraName);
             }
         }
+
+        //变更装备名称
+        public void changeEquipName(Text text, int itemId)
+        {
+            if (!Main.settings.showExtraName) return;
+            int typ2 = getItemSecondType(itemId);
+            switch (typ2)
+            {
+                case 13: //宝物
+                    changeDecName(text, itemId);
+                    break;
+            }
+
+        }
+
+        //变更图纸名称
+        public void changeBlueprintName(Text text, int itemId)
+        {
+            if (!Main.settings.showBlueprintName) return;
+            string name = getItemName(itemId);
+            changeText(text, name);
+        }
+
+        //变更书籍名称
+        public void changeBookName(Text text, int itemId)
+        {
+            if (!Main.settings.showBookGang) return;
+            int typ3 = getItemThirdType(itemId);
+            //4-19为技艺，20-33为功法
+            if (typ3 >= 20)
+            {
+                int gongFaId = getGongFaId(itemId);
+                string gangName = getGangName(gongFaId);
+                text.text = gangName + "\n" + text.text;
+                //changeText(text, gangName);
+            }
+        }
+
+        //变更图书与图纸名称
+        public void changeType5Name(Text text, int itemId)
+        {
+            int typ2 = getItemSecondType(itemId);
+            switch (typ2)
+            {
+                case 20:
+                    changeBlueprintName(text, itemId);
+                    break;
+                case 21:
+                    changeBookName(text, itemId);
+                    break;
+            }
+        }
+
         //变更物品名称
-        public void changeItemName(Text text,int itemId)
+        public void changeItemName(Text text, int itemId)
         {
             int typ = this.getItemType(itemId);
             switch (typ)
             {
-                case 3:
-                    changeDecName(text, itemId);
+                case 4:
+                    changeEquipName(text, itemId);
+                    break;
+                case 5:
+                    changeType5Name(text, itemId);
                     break;
             }
-            
+
         }
     }
 
@@ -260,7 +329,7 @@ namespace MoreInfo
     {
         static void Postfix(SetItem __instance, int actorId, int itemId, int actorFavor, int injuryTyp)
         {
-            if (!Main.enabled || !Main.settings.showItemExtraInBag)
+            if (!Main.enabled || !Main.settings.showInBag)
                 return;
             Changer changer = new Changer();
             changer.changeItemName(__instance.itemNumber, itemId);
@@ -273,7 +342,7 @@ namespace MoreInfo
     {
         static void Postfix(SetItem __instance, int itemId)
         {
-            if (!Main.enabled || !Main.settings.showItemExtraInEquuipBag)
+            if (!Main.enabled || !Main.settings.showInEquuipBag)
                 return;
             Changer changer = new Changer();
             changer.changeItemName(__instance.itemNumber, itemId);
@@ -287,11 +356,11 @@ namespace MoreInfo
     {
         static void Postfix(SetItem __instance, int actorId, int itemId, bool cantTake, Image itemDragDes = null, int itemDragTyp = -1)
         {
-            if (!Main.enabled || !Main.settings.showItemExtraInBank)
+            if (!Main.enabled || !Main.settings.showInBank)
                 return;
             Changer changer = new Changer();
-            int typ = changer.getItemType(itemId);
             changer.changeItemName(__instance.itemNumber, itemId);
+
         }
     }
 
@@ -301,7 +370,7 @@ namespace MoreInfo
     {
         static void Postfix(SetItem __instance, int itemId, int itemSize, int desIndex, int changeTyp, int itemMoney, Image dragDes)
         {
-            if (!Main.enabled || !Main.settings.showItemExtraInShop)
+            if (!Main.enabled || !Main.settings.showInShop)
                 return;
             Changer changer = new Changer();
             changer.changeItemName(__instance.itemNumber, itemId);
@@ -318,13 +387,16 @@ namespace MoreInfo
         {
             if (!Main.enabled)
                 return;
+
+            Changer changer = new Changer();
+
             //功法所属门派
             if (Main.settings.showGongFaGang)
             {
-                int gangId = int.Parse(DateFile.instance.gongFaDate[gongFaId][3]);
-                string gangName = DateFile.instance.presetGangDate[gangId][0];
+                string gangName = changer.getGangName(gongFaId);
                 __instance.gongFaSizeText.text = gangName + "\n" + __instance.gongFaSizeText.text;
             }
+
             //根据修习进度与心得变更颜色增加区分度
             if (Main.settings.showGongFaProgress)
             {
@@ -376,8 +448,8 @@ namespace MoreInfo
             int partId = df.mianPartId;
             if (df.HaveShow(partId, pid) <= 0) return;
             if (!DateFile.instance.HaveStory(partId, pid)) return;
-
             if (!df.worldMapState[partId].ContainsKey(pid)) return;
+
             int storyId = df.worldMapState[partId][pid][0];
             string level = df.baseStoryDate[storyId][3];
 
