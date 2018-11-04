c1ff5b49c4fb068293c96855e3d03591d0aff5eb
gdjyhjs <996776472@qq.com>
2018-11-05 01:06:54 +0800

    忽略

--- genvsproj.cmd
+++ genvsproj.cmd
@@ -1,7 +1,10 @@
 ::set STEAMDIR="C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Scroll Of Taiwu\\"
 ::set STEAMDIR="E:\\SteamLibrarys\\steamapps\\common\\The Scroll Of Taiwu\\"
-set STEAMDIR="D:\\Software\\steam\\steamapps\\common\\The Scroll Of Taiwu\\"
+set STEAMDIR="G:\\steam\\steamapps\\common\\The Scroll Of Taiwu\\"
 mkdir build
 cd build
 
 cmake -G "Visual Studio 15 2017" -D STEAMDIR=%STEAMDIR%  ..
+
+
+pause
