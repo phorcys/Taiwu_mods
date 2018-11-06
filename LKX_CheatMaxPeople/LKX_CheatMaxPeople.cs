a4cfd73f30562c19691e38d039c80ab64c0d825c
linkaixiang4883 <linkaixiang4883@qq.com>
2018-11-06 12:51:39 +0800

    Add 作弊人口上限
    加个人口作弊。

---
+++ LKX_CheatMaxPeople/LKX_CheatMaxPeople.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony12;
+using UnityModManagerNet;
+using UnityEngine;
+
+namespace LKX_CheatMaxPeople
+{
+    /// <summary>
+    /// 设置文件
+    /// </summary>
+    public class Settings : UnityModManager.ModSettings
+    {
+        /// <summary>
+        /// GUI选择的类型
+        /// </summary>
+        public int maxPeople;
+
+        /// <summary>
+        /// 保存设置
+        /// </summary>
+        /// <param name="modEntry"></param>
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+
+    public class Main
+    {
+        /// <summary>
+        /// umm日志
+        /// </summary>
+        public static UnityModManager.ModEntry.ModLogger logger;
+
+        /// <summary>
+        /// mod设置
+        /// </summary>
+        public static Settings settings;
+
+        /// <summary>
+        /// 是否开启mod
+        /// </summary>
+        public static bool enabled;
+
+        /// <summary>
+        /// 载入mod。
+        /// </summary>
+        /// <param name="modEntry">mod管理器对象</param>
+        /// <returns></returns>
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Main.logger = modEntry.Logger;
+            Main.settings = Settings.Load<Settings>(modEntry);
+
+            HarmonyInstance.Create(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+
+            modEntry.OnToggle = Main.OnToggle;
+            modEntry.OnGUI = Main.OnGUI;
+            modEntry.OnSaveGUI = Main.OnSaveGUI;
+
+            return true;
+        }
+
+        /// <summary>
+        /// 确定是否激活mod
+        /// </summary>
+        /// <param name="modEntry">umm</param>
+        /// <param name="value">是否激活</param>
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Main.enabled = value;
+            return true;
+        }
+
+        /// <summary>
+        /// 展示mod的设置
+        /// </summary>
+        /// <param name="modEntry">umm</param>
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUIStyle redLabelStyle = new GUIStyle();
+            redLabelStyle.normal.textColor = new Color(159f / 256f, 20f / 256f, 29f / 256f);
+            GUILayout.Label("修改人力上限！", redLabelStyle);
+
+            string maxPeople = GUILayout.TextField(Main.settings.maxPeople.ToString());
+            if (GUI.changed)
+            {
+                if (string.Empty == maxPeople) maxPeople = "0";
+
+                Main.settings.maxPeople = int.Parse(maxPeople);
+            }
+
+        }
+
+        /// <summary>
+        /// 保存mod的设置
+        /// </summary>
+        /// <param name="modEntry">umm</param>
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Main.settings.Save(modEntry);
+        }
+    }
+
+    [HarmonyPatch(typeof(UIDate), "GetMaxManpower")]
+    public class MaxPeople_For_UIDate_GetBaseMaxManpower
+    {
+        static void Postfix(ref int __result)
+        {
+            if (Main.enabled && Main.settings.maxPeople >= 0) __result += Main.settings.maxPeople;
+        }
+    }
+}
---
+++ LKX_CheatMaxPeople/README.md
@@ -0,0 +1,14 @@
+# LKX_GongFaSizeToOne
+太吾绘卷的mod，给上限和可用的人口再加上自己想加的数值
+# 使用方法
+需要Visual Studio 2017  
+需要Unity3D一堆dll  
+需要UnityModManager里的0Harmony12.dll  
+需要QQ群903110962里的Assembly-CSharp的dll  
+# MOD说明
+* 单纯人口修改，给群里研究用吧。想搬的就搬吧。
+# 安装说明
+* 使用UnityModManager安装
+* 编译后打包dll文件以及Info.json文件和pdb文件打包成zip，拖入UnityModManager安装
+* 太吾V0.1.4或以上版本使用UnityModManager V0.12.2版安装
+* 太吾V0.1.4以下版本使用UnityModManager V0.12.0-V0.12.1版本安装
