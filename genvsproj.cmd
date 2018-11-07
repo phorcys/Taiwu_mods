50d8489a06a0394ec77a740409f4052acbe190df
gdjyhjs <996776472@qq.com>
2018-11-07 16:12:26 +0800

    还原设置

--- genvsproj.cmd
+++ genvsproj.cmd
@@ -1,6 +1,5 @@
 ::set STEAMDIR="C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Scroll Of Taiwu\\"
-::set STEAMDIR="D:\\Software\\steam\\steamapps\\common\\The Scroll Of Taiwu\\"
-set STEAMDIR="D:\\Software\\steam\\steamapps\\common\\The Scroll Of Taiwu\\"
+set STEAMDIR="E:\\SteamLibrarys\\steamapps\\common\\The Scroll Of Taiwu\\"
 mkdir build
 cd build
 
