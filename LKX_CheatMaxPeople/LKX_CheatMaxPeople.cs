86f2b8e47c04edc2584b5b3469e5940b4434b9da
linkaixiang4883 <linkaixiang4883@qq.com>
2018-11-16 19:54:08 +0800

    删除昆仑·镜冢的代码
    恢复成我喜爱的作弊

--- LKX_CheatMaxPeople/LKX_CheatMaxPeople.cs
+++ LKX_CheatMaxPeople/LKX_CheatMaxPeople.cs
@@ -92,7 +92,6 @@ namespace LKX_CheatMaxPeople
             modEntry.OnGUI = Main.OnGUI;
             modEntry.OnSaveGUI = Main.OnSaveGUI;
 
-            
             return true;
         }
 
@@ -207,32 +206,6 @@ namespace LKX_CheatMaxPeople
             }
             GUILayout.EndHorizontal();
 
-            GUILayout.BeginHorizontal("Box", new GUILayoutOption[0]);
-            if (GUILayout.Button("测试事件"))
-            {
-                DateFile.instance.SetEvent(new int[] { 0, -1, 16785 }, false);
-            }
-            if (GUILayout.Button("测试地图资源"))
-            {
-                Main.SetStoryForKunLun();
-            }
-            if (GUILayout.Button("检测地图资源"))
-            {
-                Main.GetStoryForKunLun();
-            }
-            if (GUILayout.Button("检测地图资源1"))
-            {
-                if (DateFile.instance.worldMapState[DateFile.instance.mianPartId].ContainsKey(DateFile.instance.mianPlaceId))
-                {
-                    Main.logger.Log(DateFile.instance.worldMapState[DateFile.instance.mianPartId][DateFile.instance.mianPlaceId].Length.ToString());
-                }
-                else
-                {
-                    Main.logger.Log("不存在的");
-                }
-            }
-            GUILayout.EndHorizontal();
-
             if (Main.teamActorId.Count > 0)
             {
                 string actorId = "";
@@ -303,7 +276,7 @@ namespace LKX_CheatMaxPeople
                 }
             }
         }
-        
+
         static void TestModValue()
         {
             DateFile df = DateFile.instance;
@@ -359,98 +332,6 @@ namespace LKX_CheatMaxPeople
                 Main.logger.Log("恢复失败，请重新退出游戏。");
             }
         }
-
-        public static void SetStoryForKunLun()
-        {
-            int partId = int.Parse(DateFile.instance.GetGangDate(16, 3));
-            int placeId = int.Parse(DateFile.instance.GetGangDate(16, 4));
-            List<int> list = new List<int>(GetTaiWuHomePlaceList(partId, placeId, false));
-
-            if (list.Count > 0)
-            {
-                int placeId2 = list[UnityEngine.Random.Range(0, list.Count)];
-                DateFile.instance.SetStory(true, partId, placeId2, 200001, -1);
-            }
-            else
-            {
-                Main.logger.Log("添加失败，太吾村所在地图已经没有位置。");
-            }
-        }
-
-        public static void GetStoryForKunLun()
-        {
-            int partId = int.Parse(DateFile.instance.GetGangDate(16, 3));
-            int placeId = int.Parse(DateFile.instance.GetGangDate(16, 4));
-            List<int> list = new List<int>(GetTaiWuHomePlaceList(partId, placeId));
-
-            if (list.Count > 0)
-            {
-                int i = 0;
-                foreach(int storyKey in list)
-                {
-                    if (DateFile.instance.worldMapState[partId][storyKey][0] == 200001)
-                    {
-                        DateFile.instance.SetStory(false, partId, storyKey, 0, -1);
-                        i++;
-                    }
-                }
-
-                if (i > 0)
-                {
-                    Main.logger.Log("完成卸载！卸载了：" + i.ToString() + "个昆仑·镜冢.");
-                    return;
-                }
-            }
-
-            Main.logger.Log("没有找到昆仑·镜冢，可能已经卸载完成过或者该冢并没有出现过，建议备份存档。");
-        }
-
-        public static List<int> GetTaiWuHomePlaceList(int partId, int placeId, bool isStory = true)
-        {
-            List<int> list = new List<int>();
-            List<int> list2 = new List<int>(DateFile.instance.GetWorldMapNeighbor(partId, placeId, 1));
-            List<int> list3 = new List<int>(DateFile.instance.GetWorldMapNeighbor(partId, placeId, 10));
-            for (int j = 0; j < list3.Count; j++)
-            {
-                int num2 = list3[j];
-                bool flag;
-                if (isStory)
-                {
-                    flag = !list2.Contains(num2) && DateFile.instance.HaveStory(partId, num2) && int.Parse(DateFile.instance.GetNewMapDate(partId, num2, 93)) == 0;
-                }
-                else
-                {
-                    flag = !list2.Contains(num2) && !DateFile.instance.HaveStory(partId, num2) && int.Parse(DateFile.instance.GetNewMapDate(partId, num2, 93)) == 0;
-                }
-
-                if (flag)
-                {
-                    list.Add(num2);
-                }
-            }
-
-            return list;
-        }
-
-        public static bool KunLunIsExist()
-        {
-            int partId = int.Parse(DateFile.instance.GetGangDate(16, 3));
-            int placeId = int.Parse(DateFile.instance.GetGangDate(16, 4));
-            List<int> list = new List<int>(GetTaiWuHomePlaceList(partId, placeId));
-
-            if (list.Count > 0)
-            {
-                foreach (int storyKey in list)
-                {
-                    if (DateFile.instance.worldMapState[partId][storyKey][0] == 200001)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
     }
 
     [HarmonyPatch(typeof(UIDate), "GetMaxManpower")]
@@ -473,7 +354,6 @@ namespace LKX_CheatMaxPeople
             {
                 if (Main.settings.ququLife) QuQuCheat();
                 if (Main.settings.foodMerge) FoodMergeCheat();
-                if (DateFile.instance.GetWorldXXLevel() >= 7 && !Main.KunLunIsExist()) DateFile.instance.SetEvent(new int[] { 0, -1, 16785 }, false);
             }
         }
 
@@ -618,213 +498,6 @@ namespace LKX_CheatMaxPeople
         static void Postfix(ref int __result)
         {
             if (Main.enabled && Main.settings.cheatWarehouseMaxSize) __result = 100000000;
-        }
-    }
-
-    [HarmonyPatch(typeof(DateFile), "MakeXXEnemy")]
-    public class LKX_Enemy_For_DateFile_MakeXXEnemy
-    {
-        static void Prefix(int baseActorId, int index)
-        {
-            if (baseActorId == 18628)
-            {
-                int num = Mathf.Min(index + 1, 10);
-                for(int i = 0; i < 7; i++)
-                {
-                    int equipId = int.Parse(DateFile.instance.presetActorDate[18628][304 + i]);
-                    if(equipId != 0)
-                    {
-                        DateFile.instance.presetActorDate[18628][304 + i] = (equipId - num / 2).ToString();
-                    }
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(DateFile), "SetEvent")]
-    public class LKX_RegEvent_For_DateFile_SetEvent
-    {
-        static void Prefix(int[] eventDate)
-        {
-            if (eventDate[2] == 16781)
-            {
-                Main.logger.Log("进入对话：" + eventDate[2].ToString());
-                LKX_BossBattle_MassageWindow_EndEvent147_1.RunActorData();
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(MassageWindow), "EndEvent")]
-    public class LKX_RegEvent_For_MassageWindow_EndEvent
-    {
-        static void Prefix()
-        {
-            if (MassageWindow.instance.eventValue.Count > 0 && MassageWindow.instance.eventValue[0] != 0)
-            {
-                Main.logger.Log(MassageWindow.instance.eventValue[0].ToString());
-                if (MassageWindow.instance.eventValue[0] == 16785)
-                {
-                    Main.logger.Log("进入16785");
-                    Main.SetStoryForKunLun();
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(MassageWindow), "DoEvent")]
-    public class LKX_RegEvent_For_MassageWindow_DoEvent
-    {
-        static void Prefix(int eventIndex)
-        {
-            Main.logger.Log("DoEvent:{" + eventIndex.ToString() + "}");
-        }
-    }
-
-    [HarmonyPatch(typeof(MassageWindow), "SetMassageWindow")]
-    public class LKX_RegEvent_For_MassageWindow_SetMassageWindow
-    {
-        static void Prefix(int[] baseEventDate, int chooseId)
-        {
-            Main.logger.Log("SetMassageWindow:{");
-            Main.logger.Log("  chooseId:" + chooseId.ToString());
-            Main.logger.Log("  baseEventDate:{");
-            for(int i = 0; i < baseEventDate.Length; i++)
-            {
-                Main.logger.Log("    " + i.ToString() + ":" + baseEventDate[i].ToString());
-            }
-            Main.logger.Log("  }");
-            Main.logger.Log("}");
-        }
-    }
-
-    [HarmonyPatch(typeof(MassageWindow), "EndEventChangeMassageWindow")]
-    public class LKX_RegEvent_For_MassageWindow_EndEventChangeMassageWindow
-    {
-        public static bool active = false;
-        static void Prefix(int eventId)
-        {
-            if (eventId == 110)
-            {
-                active = true;
-            }
-        }
-
-        static void Postfix()
-        {
-            if (active)
-            {
-                DateFile df = DateFile.instance;
-                df.SetStory(true, df.mianPartId, df.mianPlaceId, Main.settings.eventId);
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(MassageWindow), "EndEvent147_1")]
-    public class LKX_BossBattle_MassageWindow_EndEvent147_1
-    {
-        static bool Prefix()
-        {
-            //长度2，index{0:事件id  1:对战人物id}
-            int eventIndex = MassageWindow.instance.eventValue[1];
-            if (eventIndex == 18628)
-            {
-                Event_StartBattleWindow();
-                return false;
-            }
-
-            return true;
-        }
-
-        public static void Event_StartBattleWindow()
-        {
-            int teamId = 200001;
-
-            StartBattle.instance.ShowStartBattleWindow(teamId, 0, 18, new List<int> { MassageWindow.instance.eventValue[1] });
-        }
-
-        public static void RunActorData()
-        {
-            int id = DateFile.instance.MianActorID();
-            Dictionary<int, string> pactor = new Dictionary<int, string>();
-            Dictionary<int, string> mianactor = new Dictionary<int, string>();
-            if (DateFile.instance.presetActorDate.TryGetValue(18628, out pactor) && DateFile.instance.actorsDate.TryGetValue(DateFile.instance.mianActorId, out mianactor))
-            {
-                SortedDictionary<int, int[]> mianGongFa = new SortedDictionary<int, int[]>();
-                if (DateFile.instance.actorGongFas.TryGetValue(id, out mianGongFa))
-                {
-                    pactor[906] = GongFaMerge(mianGongFa);
-                }
-                
-                foreach (KeyValuePair<int, string> actor in mianactor)
-                {
-                    List<int> equip = new List<int> { 304, 305, 306, 307, 308, 309, 310, 311 };
-                    if (equip.Contains(actor.Key))
-                    {
-                        pactor[actor.Key] = DateFile.instance.GetItemDate(int.Parse(actor.Value), 999);
-                    }
-
-                    if (pactor.ContainsKey(actor.Key))
-                    {
-
-                        List<int> exist = new List<int> {
-                            5,11,17,
-                            61,62,63,64,65,66,
-                            71,72,73,
-                            81,82,83,84,85,86,
-                            92,93,94,95,96,97,98,
-                            101,
-                            501,502,503,504,505,506,507,508,509,510,511,512,513,514,515,516,
-                            601,602,603,604,605,606,607,608,609,610,611,612,613,614,
-                            651,551,
-                            995,996
-                        };
-
-                        if (!exist.Contains(actor.Key)) continue;
-                        
-                        if (actor.Key == 101)
-                        {
-                            pactor[actor.Key] = "10011|" + actor.Value;
-                        }
-                        else if(actor.Key == 5)
-                        {
-                            //暂时0是有名字的
-                            pactor[0] = "昆仑镜·" + mianactor[actor.Key];
-                            if (mianactor[0] != "NoSurname" || mianactor[0] != "无名" || mianactor[0] != "") pactor[0] = pactor[0] + mianactor[0];
-                        }
-                        else
-                        {
-                            pactor[actor.Key] = actor.Value;
-                        }
-                    }
-                }
-            }
-        }
-
-        public static string GongFaMerge(SortedDictionary<int, int[]> gongfa)
-        {
-            string defaultGongFa = "170010&0";
-            foreach(KeyValuePair<int, int[]> item in gongfa)
-            {
-                if(item.Value[0] >= 25)
-                {
-                    string addGongFa = "";
-                    if(item.Value[2] == 0)
-                    {
-                        addGongFa = "|" + item.Key.ToString() + "&0";
-                    }
-                    else if(item.Value[2] <= 5 && item.Value[2] != 0)
-                    {
-                        addGongFa = "|" + item.Key.ToString() + "&1";
-                    }
-                    else
-                    {
-                        addGongFa = "|" + item.Key.ToString() + "&2";
-                    }
-                    defaultGongFa += addGongFa;
-                }
-            }
-
-            return defaultGongFa;
         }
     }
 }
