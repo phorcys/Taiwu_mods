352192cc271b81c6926198fab3d5e87f354a4dd6
gdjyhjs <996776472@qq.com>
2019-02-27 00:23:39 +0800

    更新今天代码

--- GuiMartialArts/GuiMartialArts.cs
+++ GuiMartialArts/GuiMartialArts.cs
@@ -75,7 +75,7 @@ namespace GuiMartialArts
 
 
         [HarmonyPatch(typeof(BattleSystem), "UseGongFaStart")]
-        public static class SetPlaceActor_ShowEventMassage_Patch
+        public static class BattleSystem_UseGongFaStart_Patch
         {
             public static void Postfix(bool isActor, int gongFaId)
             {
@@ -91,7 +91,7 @@ namespace GuiMartialArts
         }
 
         [HarmonyPatch(typeof(BattleSystem), "UseGongFa")]
-        public static class WorldMapSystem_RemoveActor_Patch1
+        public static class BattleSystem_UseGongFa_Patch1
         {
             public static void Postfix(bool isActor, int gongFaId)
             {
@@ -107,7 +107,7 @@ namespace GuiMartialArts
         }
 
         [HarmonyPatch(typeof(BattleSystem), "BattleEnd")]
-        public static class WorldMapSystem_UpdatePlaceActor_Patch1
+        public static class BattleSystem_BattleEnd_Patch1
         {
             public static void Postfix(bool actorWin, int actorRun = 0)
             {
@@ -117,6 +117,190 @@ namespace GuiMartialArts
                 Logger.Log("战斗结束 actorWin=" + actorWin + " actorRun=" + actorRun);
 
                 artsData.SaveWindows();
+
+                return;
+            }
+        }
+
+        [HarmonyPatch(typeof(BattleSystem), "ChangeEnemy")]
+        public static class BattleSystem_ChangeEnemy_Patch1
+        {
+            public static void Postfix(int index)
+            {
+                if (!Main.enabled)
+                    return;
+
+                int _newEnemyId = DateFile.instance.enemyBattlerIdDate[index];
+                int _oldEnemyId = DateFile.instance.enemyBattlerIdDate[0];
+                Logger.Log("更换敌人 index=" + index + " _newEnemyId=" + _newEnemyId + " _oldEnemyId=" + _oldEnemyId);
+                artsData.AddBattleEnemy(_newEnemyId, _oldEnemyId);
+
+                return;
+            }
+        }
+
+
+
+
+        [HarmonyPatch(typeof(ReadBook), "GetReadBooty")]
+        public static class ReadBook_GetReadBooty_Patch1
+        {
+            public static void Postfix()
+            {
+                if (!Main.enabled)
+                    return;
+
+                ReadBook __this = ReadBook.instance;
+                var _readPageIndex = typeof(ReadBook).GetField("readPageIndex", BindingFlags.NonPublic | BindingFlags.Instance);
+                var __readPageIndex = (int)_readPageIndex.GetValue(ReadBook.instance);
+
+
+                var _pageState = typeof(ReadBook).GetField("pageState", BindingFlags.NonPublic | BindingFlags.Instance);
+                var __pageState = (List<int[]>)_pageState.GetValue(ReadBook.instance);
+
+
+                int num = DateFile.instance.MianActorID();
+                Logger.Log("num " + num);
+                int num2 = 100;
+                for (int i = 0; i < 3; i++)
+                {
+                    int key = __pageState[__readPageIndex][i];
+                    num2 += int.Parse(DateFile.instance.readBookDate[key][6]); // 6是奖励 // 6是奖励
+                }
+
+                int num3 = int.Parse(DateFile.instance.GetItemDate(HomeSystem.instance.readBookId, 32, true)); // 功法id
+                Logger.Log("功法idnum3 " + num3);
+                int num4 = int.Parse(DateFile.instance.GetItemDate(HomeSystem.instance.readBookId, 34, true)) * num2 / 100; // 分数
+                Logger.Log("分数num4 " + num4);
+                int[] bookPage = DateFile.instance.GetBookPage(HomeSystem.instance.readBookId);
+                bool flag = HomeSystem.instance.studySkillTyp == 17;
+                Logger.Log("flag " + flag);
+                if (flag) // 读的是功法书
+                {
+                    Logger.Log("读的是功法书");
+                    bool flag2 = !DateFile.instance.gongFaBookPages.ContainsKey(num3);
+                    Logger.Log("flag2 " + flag2);
+                    if (flag2)
+                    {
+                        DateFile.instance.gongFaBookPages.Add(num3, new int[10]);
+                    }
+                    int num5 = DateFile.instance.gongFaBookPages[num3][__readPageIndex];
+                    Logger.Log("num5 " + num5);
+                    bool flag3 = num5 != 1 && num5 > -100;
+                    Logger.Log("flag3 " + flag3);
+                    if (flag3)
+                    {
+                        int num6 = int.Parse(DateFile.instance.gongFaDate[num3][2]);
+                        Logger.Log("num6 " + num6);
+                        int num7 = int.Parse(DateFile.instance.GetItemDate(HomeSystem.instance.readBookId, 35, true));
+                        Logger.Log("num7 " + num7);
+                        DateFile.instance.ChangeActorGongFa(num, num3, 0, 0, num7, true);
+                        bool flag4 = !DateFile.instance.actorGongFas[num].ContainsKey(num3);
+                        Logger.Log("flag4 " + flag4);
+                        if (flag4)
+                        {
+                            __this.getSkillWindow.SetActive(true);
+                            __this.getSkillNameText.text = DateFile.instance.massageDate[7010][3] + WindowManage.instance.Mut() + DateFile.instance.gongFaDate[num3][0];
+                            //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getSkillNameText.GetComponent<RectTransform>(), new Vector3(1.8f, 1.8f, 1f), 0.1f), true), 27);
+                            //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetDelay<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getSkillNameText.GetComponent<RectTransform>(), new Vector3(1f, 1f, 1f), 0.4f), true), 0.1f), 27);
+                        }
+                        bool flag5 = num7 != 0;
+                        Logger.Log("flag5 " + flag5);
+                        if (flag5)
+                        {
+                            ActorMenu.instance.ChangeMianQi(num, 50 * num6, 5);
+                            TipsWindow.instance.SetTips(0, new string[]
+                            {
+                        DateFile.instance.readBookDate[105][98]
+                            }, 180, -730f, -310f, 450, 100);
+                        }
+                        else
+                        {
+                            TipsWindow.instance.SetTips(0, new string[]
+                            {
+                        DateFile.instance.readBookDate[102][98]
+                            }, 180, -730f, -310f, 450, 100);
+                        }
+                        __pageState[__readPageIndex][5] = 1;
+                        DateFile.instance.gongFaBookPages[num3][__readPageIndex] = 1;
+                        //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getFlevelText.GetComponent<RectTransform>(), new Vector3(1.8f, 1.8f, 1f), 0.1f), true), 27);
+                        //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetDelay<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getFlevelText.GetComponent<RectTransform>(), new Vector3(1f, 1f, 1f), 0.4f), true), 0.1f), 27);
+                        DateFile.instance.AddActorScore(303, num6 * 100);
+                        bool flag6 = DateFile.instance.GetGongFaLevel(num, num3, 0) >= 100 && DateFile.instance.GetGongFaFLevel(num, num3, false) >= 10;
+                        Logger.Log("flag6 " + flag6);
+                        if (flag6)
+                        {
+                            DateFile.instance.AddActorScore(304, num6 * 100);
+                        }
+                        bool flag7 = bookPage[__readPageIndex] == 0;
+                        Logger.Log("flag7 " + flag7);
+                        if (flag7)
+                        {
+                            DateFile.instance.AddActorScore(305, num6 * 100);
+                        }
+                    }
+                    else
+                    {
+                        num4 = num4 * 10 / 100;
+                        TipsWindow.instance.SetTips(0, new string[]
+                        {
+                    DateFile.instance.readBookDate[102][98]
+                        }, 180, -730f, -310f, 450, 100);
+                    }
+                    DateFile.instance.gongFaExperienceP += num4;
+                }
+                else // 读的是技艺书
+                {
+                    bool flag8 = !DateFile.instance.skillBookPages.ContainsKey(num3);
+                    if (flag8)
+                    {
+                        DateFile.instance.skillBookPages.Add(num3, new int[10]);
+                    }
+                    int num8 = DateFile.instance.skillBookPages[num3][__readPageIndex];
+                    bool flag9 = num8 != 1 && num8 > -100;
+                    if (flag9)
+                    {
+                        int num9 = int.Parse(DateFile.instance.skillDate[num3][2]);
+                        bool flag10 = !DateFile.instance.actorSkills.ContainsKey(num3);
+                        if (flag10)
+                        {
+                            DateFile.instance.ChangeMianSkill(num3, 0, 0, true);
+                            __this.getSkillWindow.SetActive(true);
+                            __this.getSkillNameText.text = DateFile.instance.massageDate[7010][3] + WindowManage.instance.Mut() + DateFile.instance.skillDate[num3][0];
+                            //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getSkillNameText.GetComponent<RectTransform>(), new Vector3(1.8f, 1.8f, 1f), 0.1f), true), 27);
+                            //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetDelay<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getSkillNameText.GetComponent<RectTransform>(), new Vector3(1f, 1f, 1f), 0.4f), true), 0.1f), 27);
+                        }
+                        __pageState[__readPageIndex][5] = 1;
+                        DateFile.instance.skillBookPages[num3][__readPageIndex] = 1;
+                        //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getFlevelText.GetComponent<RectTransform>(), new Vector3(1.8f, 1.8f, 1f), 0.1f), true), 27);
+                        //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetDelay<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getFlevelText.GetComponent<RectTransform>(), new Vector3(1f, 1f, 1f), 0.4f), true), 0.1f), 27);
+                        DateFile.instance.AddActorScore(203, num9 * 100);
+                        bool flag11 = DateFile.instance.GetSkillLevel(num3) >= 100 && DateFile.instance.GetSkillFLevel(num3) >= 10;
+                        if (flag11)
+                        {
+                            DateFile.instance.AddActorScore(204, num9 * 100);
+                        }
+                        bool flag12 = bookPage[__readPageIndex] == 0;
+                        if (flag12)
+                        {
+                            DateFile.instance.AddActorScore(205, num9 * 100);
+                        }
+                    }
+                    else
+                    {
+                        num4 = num4 * 10 / 100;
+                    }
+                    TipsWindow.instance.SetTips(0, new string[]
+                    {
+                DateFile.instance.readBookDate[102][98]
+                    }, 180, -730f, -310f, 450, 100);
+                    DateFile.instance.gongFaExperienceP += num4;
+                }
+                __pageState[__readPageIndex][4] += num4;
+                //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getExpText.GetComponent<RectTransform>(), new Vector3(1.8f, 1.8f, 1f), 0.1f), true), 27);
+                //TweenSettingsExtensions.SetEase<Tweener>(TweenSettingsExtensions.SetDelay<Tweener>(TweenSettingsExtensions.SetUpdate<Tweener>(ShortcutExtensions.DOScale(__this.getExpText.GetComponent<RectTransform>(), new Vector3(1f, 1f, 1f), 0.4f), true), 0.1f), 27);
+                //__this.UpdatePageText();
+                //__this.UpdateGetP();
 
                 return;
             }
--- GuiMartialArts/MartialArtsData.cs
+++ GuiMartialArts/MartialArtsData.cs
@@ -70,6 +70,7 @@ namespace GuiMartialArts
                     GongfaProficiency[gongFaId]++;
                 else
                     GongfaProficiency.Add(gongFaId, 1);
+                actorUseGongfaTimes++;
             }
             else
             {
@@ -77,9 +78,22 @@ namespace GuiMartialArts
                     BattleGongfaWeitht[gongFaId] += 1;
                 else
                     BattleGongfaWeitht.Add(gongFaId, 2);
+                enemyUseGongfaTimes++;
             }
         }
 
+
+        public void AddBattleEnemy(int _newEnemyId, int _oldEnemyId)
+        {
+            if (!BattleEnemyIds.Contains(_newEnemyId))
+                BattleEnemyIds.Add(_newEnemyId);
+
+            if (!BattleEnemyIds.Contains(_oldEnemyId))
+                BattleEnemyIds.Add(_oldEnemyId);
+        }
+
+        int enemyUseGongfaTimes = 0;
+        int actorUseGongfaTimes = 0;
 
         string winTitle = "获得战斗领悟";
         string winMessage = "你在战斗中偷窥到了对方的心法，是否静心参悟？";
@@ -88,31 +102,30 @@ namespace GuiMartialArts
             Main.Logger.Log("弹出领悟功法窗口:");
             YesOrNoWindow.instance.SetYesOrNoWindow(1992062500, winTitle, winMessage, true, true);
             Button okbtn = YesOrNoWindow.instance.yesOrNoWindow.Find("YesButton").GetComponent<Button>();
-            okbtn.onClick.AddListener(SaveData);
+            okbtn.onClick.AddListener(ClickYes);
             Button nobtn = YesOrNoWindow.instance.yesOrNoWindow.Find("NoButton").GetComponent<Button>();
-            nobtn.onClick.AddListener(DiscardData);
+            nobtn.onClick.AddListener(ClickNo);
         }
 
-        private void SaveData()
+        private void ClickYes()
         {
             int id = OnClick.instance.ID;
             Main.Logger.Log("点击了确认按钮:" + id);
             if (id == 1992062500)
             {
-                Main.Logger.Log("保存数据");
+                CalculationData();
+                RemoveBind();
             }
-            RemoveBind();
         }
 
-        private void DiscardData()
+        private void ClickNo()
         {
             int id = OnClick.instance.ID;
             Main.Logger.Log("点击了取消按钮:" + id);
             if (id == 1992062500)
             {
-                Main.Logger.Log("丢弃数据");
+                RemoveBind();
             }
-            RemoveBind();
         }
 
         private void RemoveBind()
@@ -121,6 +134,109 @@ namespace GuiMartialArts
             okbtn.onClick.RemoveAllListeners();
             Button nobtn = YesOrNoWindow.instance.yesOrNoWindow.Find("NoButton").GetComponent<Button>();
             nobtn.onClick.RemoveAllListeners();
+
+            BattleEnemyIds = null;
+            BattleGongfaWeitht = null;
+            enemyUseGongfaTimes = 0;
+            actorUseGongfaTimes = 0;
+            Main.Logger.Log("清空了数据");
+        }
+
+        private void CalculationData()
+        {
+            // 计算每个功法使用次数平均数
+            int enemyAverage = enemyUseGongfaTimes / BattleGongfaWeitht.Count;
+
+            // 此处加上装备的内功，身法，绝技
+            foreach (int enemyId in BattleEnemyIds)
+            {
+                Dictionary<int, int[]> equipGongfas = new Dictionary<int, int[]>(DateFile.instance.GetActorEquipGongFa(enemyId));
+                foreach (var GongFaIds in equipGongfas)
+                {
+                    // 0是内功 1是攻击类 2是身法类 3是护体类 4是绝技类
+                    if (GongFaIds.Key != 1 && GongFaIds.Key != 2 && GongFaIds.Key != 3)
+                        foreach (var gongFaId in GongFaIds.Value)
+                        {
+                            int times = (enemyAverage / 2 + 1);
+                            for (int i = 0; i < times; i++)
+                            {
+                                AddUseGongfa(false, gongFaId);
+                            }
+                        }
+                }
+            }
+
+            List<int> GongfaIds = new List<int>();
+            foreach (var item in BattleGongfaWeitht)
+            {
+                // 此处可能应该排除掉已经大成的功法
+                if (!GongFaIsMaxLevel(item.Key))
+                {
+                    for (int i = 0; i < item.Value; i++)
+                    {
+                        GongfaIds.Add(item.Key);
+                    }
+                }
+            }
+            // 随机得到的功法
+            int getGongfaId = GongfaIds[Random.Range(0, GongfaIds.Count)];
+
+            int addLvel = GongfaUpLevel(getGongfaId, 1);
+
+
+            Main.Logger.Log("功法等级增加 " + addLvel);
+
+        }
+
+        private bool GongFaIsMaxLevel(int gongFaId)
+        {
+            //SortedDictionary<int, int[]> sortedDictionary = DateFile.instance.actorGongFas[gongFaId];
+            //int gongFaLevel = sortedDictionary[gongFaId][0];
+            return DateFile.instance.GetGongFaLevel(DateFile.instance.mianActorId, gongFaId) >= 10;
+        }
+        
+
+        private int GongfaUpLevel(int gongFaId, int value)
+        {
+            int actorId = DateFile.instance.MianActorID(); 
+            bool flag2 = !DateFile.instance.gongFaBookPages.ContainsKey(gongFaId);
+            if (flag2)
+            {
+                DateFile.instance.gongFaBookPages.Add(gongFaId, new int[10]);
+            }
+            int[] pages = new int[value];
+            int idx = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (DateFile.instance.gongFaBookPages[gongFaId][i] == 0 && idx < value)
+                {
+                    pages[idx++] = i;
+                }
+            }
+
+            foreach (var pageIndex in pages)
+            {
+                int num5 = DateFile.instance.gongFaBookPages[gongFaId][pageIndex];
+                Main.Logger.Log("num5 " + num5);
+                bool flag3 = num5 != 1 && num5 > -100;
+                Main.Logger.Log("flag3 " + flag3);
+                if (flag3)
+                {
+
+                    int num6 = int.Parse(DateFile.instance.gongFaDate[gongFaId][2]);
+
+                    bool flag4 = !DateFile.instance.actorGongFas[actorId].ContainsKey(gongFaId);
+                    Main.Logger.Log("flag4 " + flag4);
+                    if (flag4)
+                    {
+                        string name = DateFile.instance.massageDate[7010][3] + WindowManage.instance.Mut() + DateFile.instance.gongFaDate[gongFaId][0];
+                        Main.Logger.Log("功法名称 " + name);
+                    }
+                    DateFile.instance.gongFaBookPages[gongFaId][pageIndex] = 1;
+                    DateFile.instance.AddActorScore(303, num6 * 100);
+                }
+            }
+            return idx;
         }
     }
 
