ad56071bd5074e02f9df48e8b50a8f7c334129a1
gdjyhjs <996776472@qq.com>
2019-03-01 20:15:17 +0800

    鬼的滑动

---
+++ GuiScroll/GuiScroll.cs
@@ -0,0 +1,62 @@
+﻿using Harmony12;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityModManagerNet;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace GuiWorkActor
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+        }
+        public float scrollSpeed = 10;
+        public bool openActor = true;
+    }
+    public static class Main
+    {
+        public static bool onOpen = false;//
+        public static bool enabled;
+        public static Settings settings;
+        public static UnityModManager.ModEntry.ModLogger Logger;
+
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            #region 基础设置
+            settings = Settings.Load<Settings>(modEntry);
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            #endregion
+            ActorPatch.Init(modEntry);
+
+            return true;
+        }
+
+        static string title = "鬼的滚动优化";
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.Label(title, GUILayout.Width(300));
+        }
+
+    }
+}
