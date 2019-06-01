623afe51e1331e516e3ada2f786f4e7fa703a3af
inpayhuang <inpayhuang@163.com>
2019-06-01 15:22:48 +0800

    Revert "Revert "Merge pull request #3 from phorcys/master""
    This reverts commit 09cae2080382c0edaec2867f0c3996e68a81809c.

---
+++ GuiHandle/ModUDP.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace GuiHandle
+{
+    public class ModUDP : MonoBehaviour
+    {
+        public static ModUDP instance;
+        public static List<Socket> sockets = new List<Socket>();
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public static string message;
+        private void Start()
+        {
+            Thread thread = new Thread(StartUDP);
+            thread.Start();
+        }
+
+        private void StartUDP()
+        {
+            ModReceivePage modReceivePage = new ModReceivePage();
+            modReceivePage.OnServerInitialized();
+        }
+
+        public void MyUpdate()
+        {
+            Main.Logger.Log(message);
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var item in sockets)
+            {
+                item.Close();
+            }
+        }
+    }
+
+
+
+
+    // 接收操作数据
+    public class ModReceivePage
+    {
+        public static ModReceivePage instance;
+
+        public static float left_x;
+        public static float left_y;
+        public static float right_x;
+        public static float right_y;
+        public static short boolvalue;
+        public static byte num;
+
+        public ModReceivePage()
+        {
+            if (instance != null)
+            {
+                return;
+            }
+            instance = this;
+        }
+
+        public void OnServerInitialized()
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            ModUDP.sockets.Add(sock);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 9158);
+            sock.Bind(iep);
+            while (true)
+            {
+                EndPoint ep = (EndPoint)iep;
+                byte[] data = new byte[11];
+                int recv = sock.ReceiveFrom(data, ref ep);
+
+                int idx = 0;
+                byte num = data[idx++];
+                if ((num < 75 && ModReceivePage.num > 150) || ModReceivePage.num < num)
+                {
+                    ModReceivePage.num = num;
+                    short leftx = BitConverter.ToInt16(data, 1);
+                    short lefty = BitConverter.ToInt16(data, 3);
+                    short rightx = BitConverter.ToInt16(data, 5);
+                    short righty = BitConverter.ToInt16(data, 7);
+                    boolvalue = BitConverter.ToInt16(data, 9);
+                    left_x = (float)leftx / 10000;
+                    left_y = (float)lefty / 10000;
+                    right_x = (float)rightx / 10000;
+                    right_y = (float)righty / 10000;
+
+
+                    ModUDP.message = data[0] + "\t" + data[1] + "\t" + data[2] + "\t" + data[3] + "\t" + data[4] + "\t" + data[5] + "\t" + data[6] + "\t" + data[7] + "\t" + data[8] + "\t" + data[9] + "\t" + data[10];
+                }
+                Thread.Sleep(0);
+            }
+        }
+    }
+
+
+
+
+
+
+    // 广播IP地址
+    public class ModPublicizeIp
+    {
+        private Socket sock;
+        private IPEndPoint iep1;
+        private byte[] data;
+        public void Start()
+        {
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            ModUDP.sockets.Add(sock);
+            //255.255.255.255
+            iep1 = new IPEndPoint(IPAddress.Broadcast, 9159);
+            string hostname = Dns.GetHostName();
+            data = Encoding.ASCII.GetBytes(hostname);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            Thread t = new Thread(BroadcastMessage);
+            t.Start();
+        }
+        private void BroadcastMessage()
+        {
+            while (true)
+            {
+                sock.SendTo(data, iep1);
+                Thread.Sleep(2000);
+            }
+        }
+
+        public void Close()
+        {
+            sock.Close();
+        }
+    }
+}
---
+++ GuiScroll/GuiScroll.cs
@@ -0,0 +1,84 @@
+﻿using Harmony12;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityModManagerNet;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Diagnostics;
+using UnityEngine.EventSystems;
+
+namespace GuiScroll
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            UnityModManager.ModSettings.Save<Settings>(this, modEntry);
+        }
+        public float scrollSpeed = 10;
+        public bool openActor = true;
+    }
+    public static class Main
+    {
+        public static bool onOpen = false;//
+        public static bool enabled;
+        public static Settings settings;
+        public static UnityModManager.ModEntry.ModLogger Logger;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            #region 基础设置
+            settings = Settings.Load<Settings>(modEntry);
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            #endregion
+            //ActorPatch.Init(modEntry);
+
+            ActorMenuActorListPatch.Init(modEntry);
+
+            HarmonyInstance harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            return true;
+        }
+
+        static string title = "鬼的滚动优化";
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.Label(title, GUILayout.Width(300));
+
+            //if (GUILayout.Button("测试"))
+            //{
+            //    ActorMenu.instance.listActorsHolder.root.gameObject.SetActive(!ActorMenu.instance.listActorsHolder.root.gameObject.activeSelf);
+            //    Main.Logger.Log(ActorMenu.instance.listActorsHolder.root.ToString());
+            //    Main.Logger.Log(ActorMenu.instance.listActorsHolder.ToString());
+            //}
+
+            //if (GUILayout.Button("打印"))
+            //{
+            //    GuiBaseUI.Main.LogAllChild(ActorMenu.instance.listActorsHolder.root);
+                
+            //}
+        }
+
+    }
+}
