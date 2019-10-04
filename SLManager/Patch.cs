ec82f91dc041a1532fdaef065657c6cff60c12d9
litfal <litfal@unlimiter.com.tw>
2019-10-04 13:29:53 +0800

    SLManager 1.5.5
    支援太吾 0.2.6.x
    改以負面列表方式處理存檔包含的檔案，減少未來改版造成存檔遺失的風險

--- SLManager/Patch.cs
+++ SLManager/Patch.cs
@@ -838,6 +838,11 @@ namespace Sth4nothing.SLManager
             return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString().Replace('/', Path.DirectorySeparatorChar));
         }
 
+        static readonly HashSet<string> ExcludedFileNames = new HashSet<string>()
+        {
+
+        };
+
         /// <summary>
         /// 获取备份文件列表
         /// </summary>
@@ -847,16 +852,11 @@ namespace Sth4nothing.SLManager
             var indexPath = ReflectionMethod.Invoke<string>(typeof(SaveGame), "GetSavingIndexPath", dataId);
             if (File.Exists(indexPath))
                 yield return indexPath;
-            foreach (var file in Directory.EnumerateFiles(
-                savingSubFolder,
-                "*.tw*",
-                SearchOption.TopDirectoryOnly))
+            foreach (var file in Directory.EnumerateFiles(savingSubFolder, "*", SearchOption.TopDirectoryOnly))
             {
-                yield return file;
+                if (!ExcludedFileNames.Contains(file))
+                    yield return file;
             }
-            var TW_Save_Date_9_Path = Path.Combine(savingSubFolder, "TW_Save_Date_9");
-            if (File.Exists(TW_Save_Date_9_Path))
-                yield return TW_Save_Date_9_Path;
         }
 
         /// <summary>
