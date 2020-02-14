919617462e57db34262172c2cb55a69783860845
RedContritio <RedContritio@qq.com>
2020-02-14 23:19:53 +0800

    v1.0.4，增加防绿功能

--- DreamLover/PeopleLifeAI_AISetChildren_Patch.cs
+++ DreamLover/PeopleLifeAI_AISetChildren_Patch.cs
@@ -8,83 +8,126 @@ namespace DreamLover
 	[HarmonyPatch(typeof(PeopleLifeAI), "AISetChildren")]
 	public static class PeopleLifeAI_AISetChildren_Patch
 	{
+        private static void Debug(string str) 
+        {
+            Main.Debug("<造孩子> " + str);
+        }
 		public static bool Prefix(int fatherId, int motherId, int setFather, int setMother, ref bool __result)
 		{
 			if (!Main.enabled)
 			{
 				return true;
-			}
-			if (!Main.settings.启用指定怀孕功能)
-			{
-				return true;
-			}
-			int num = DateFile.instance.MianActorID();
-			if (fatherId != num && motherId != num)
-			{
-				return true;
-			}
-			Debug.Log((object)"PrecisionShooting...");
-			int num2 = int.Parse(DateFile.instance.GetActorDate(fatherId, 24));
-			int num3 = int.Parse(DateFile.instance.GetActorDate(motherId, 24));
-			DateFile.instance.ChangeActorFeature(fatherId, 4001, 4002);
-			DateFile.instance.ChangeActorFeature(motherId, 4001, 4002);
-			GEvent.OnEvent(eEvents.Copulate, fatherId, motherId);
-			if (Main.settings.指定生育可能性 ? (Main.settings.生育可能性 <= 0) : (num2 <= 0 || num3 <= 0))
-			{
-				__result = false;
-				return false;
-			}
-			if (int.Parse(DateFile.instance.GetActorDate(motherId, 14, applyBonus: false)) != 2)
-			{
-				__result = false;
-				return false;
-			}
-			if (!DateFile.instance.HaveLifeDate(motherId, 901) && Random.Range(0, 15000) < (Main.settings.指定生育可能性 ? Main.settings.生育可能性 : (num2 * num3)))
-			{
-				Debug.Log((object)"生育可能性判定成功");
-				bool flag = fatherId == num || motherId == num;
-				int num4 = 100;
-				int num5 = flag ? 20 : 50;
-				num4 -= DateFile.instance.GetActorSocial(fatherId, 310).Count * num5;
-				num4 -= DateFile.instance.GetActorSocial(motherId, 310).Count * num5;
-				if (Random.Range(0, 100) < (Main.settings.指定怀孕概率 ? Main.settings.怀孕概率 : num4))
-				{
-					Debug.Log((object)"怀孕判定成功");
-					DateFile.instance.ChangeActorFeature(motherId, 4002, 4003);
-					if (Random.Range(0, 100) < (Main.settings.指定蛐蛐概率 ? Main.settings.蛐蛐概率 : ((DateFile.instance.getQuquTrun - 100) / 10)))
-					{
-						Debug.Log((object)"异胎判定成功");
-						DateFile.instance.getQuquTrun = 0;
-						DateFile.instance.actorLife[motherId].Add(901, new List<int>
-						{
-							1042,
-							fatherId,
-							motherId,
-							setFather,
-							setMother
-						});
-					}
-					else
-					{
-						Debug.Log((object)"常胎判定成功");
-						DateFile.instance.actorLife[motherId].Add(901, new List<int>
-						{
-							Random.Range(7, 10),
-							fatherId,
-							motherId,
-							setFather,
-							setMother
-						});
-						DateFile.instance.pregnantFeature.Add(motherId, new string[2]
-						{
-							Characters.GetCharProperty(fatherId, 101),
-							Characters.GetCharProperty(motherId, 101)
-						});
-					}
-					__result = true;
-				}
-			}
-			return false;
+            }
+            int mainActorId = DateFile.instance.MianActorID();
+
+            // 启用功能并且是主角造孩子
+            if (Main.settings.启用指定怀孕功能 && (fatherId == mainActorId || motherId == mainActorId))
+            {
+                Debug("主角尝试发生关系");
+                int num2 = int.Parse(DateFile.instance.GetActorDate(fatherId, 24));
+                int num3 = int.Parse(DateFile.instance.GetActorDate(motherId, 24));
+                DateFile.instance.ChangeActorFeature(fatherId, 4001, 4002);
+                DateFile.instance.ChangeActorFeature(motherId, 4001, 4002);
+                GEvent.OnEvent(eEvents.Copulate, fatherId, motherId);
+                Debug("父方生育可能性: " + num2 + " 母方生育可能性: " + num3);
+                if (Main.settings.指定生育可能性 ? (Main.settings.生育可能性 <= 0) : (num2 <= 0 || num3 <= 0))
+                {
+                    Debug("生育不可能，终止");
+                    __result = false;
+                    return false;
+                }
+                if (int.Parse(DateFile.instance.GetActorDate(motherId, 14, applyBonus: false)) != 2)
+                {
+                    Debug("阿拉，母方不是女性，终止");
+                    __result = false;
+                    return false;
+                }
+                int var1 = Random.Range(0, 15000);
+                if (!DateFile.instance.HaveLifeDate(motherId, 901) && var1 < (Main.settings.指定生育可能性 ? Main.settings.生育可能性 : (num2 * num3)))
+                {
+                    Debug("生育可能性判定通过，需求 " + var1 + "，结果 " + (Main.settings.指定生育可能性 ? Main.settings.生育可能性 : (num2 * num3)));
+                    bool flag = fatherId == mainActorId || motherId == mainActorId;
+                    int num4 = 100;
+                    int num5 = flag ? 20 : 50;
+                    num4 -= DateFile.instance.GetActorSocial(fatherId, 310).Count * num5;
+                    num4 -= DateFile.instance.GetActorSocial(motherId, 310).Count * num5;
+                    int var2 = Random.Range(0, 100);
+                    if (var2 < (Main.settings.指定怀孕概率 ? Main.settings.怀孕概率 : num4))
+                    {
+                        Debug("怀孕判定通过，需求 " + var2 + "，结果 " + (Main.settings.指定怀孕概率 ? Main.settings.怀孕概率 : num4));
+                        DateFile.instance.ChangeActorFeature(motherId, 4002, 4003);
+                        int var3 = Random.Range(0, 100);
+                        if (var3 < (Main.settings.指定蛐蛐概率 ? Main.settings.蛐蛐概率 : ((DateFile.instance.getQuquTrun - 100) / 10)))
+                        {
+                            Debug("异胎判定通过，需求 " + var3 + "，结果 " + (Main.settings.指定蛐蛐概率 ? Main.settings.蛐蛐概率 : ((DateFile.instance.getQuquTrun - 100) / 10)));
+                            DateFile.instance.getQuquTrun = 0;
+                            DateFile.instance.actorLife[motherId].Add(901, new List<int> { 1042, fatherId, motherId, setFather, setMother});
+                        }
+                        else
+                        {
+                            Debug("正常胎儿判定通过");
+                            DateFile.instance.actorLife[motherId].Add(901, new List<int> { Random.Range(7, 10), fatherId, motherId, setFather, setMother });
+                            DateFile.instance.pregnantFeature.Add(motherId, new string[2] {
+                                Characters.GetCharProperty(fatherId, 101),
+                                Characters.GetCharProperty(motherId, 101)
+                            });
+                        }
+                        __result = true;
+                    }
+                    else
+                    {
+                        Debug("怀孕判定未通过，需求 " + var2 + "，结果 " + (Main.settings.指定怀孕概率 ? Main.settings.怀孕概率 : num4));
+                    }
+                }
+                Debug("生育可能性判定未通过，需求 " + var1 + "，结果 " + (Main.settings.指定生育可能性 ? Main.settings.生育可能性 : (num2 * num3)));
+                return false;
+            }
+            else if(Main.settings.启用防绿功能)
+            {
+                bool 是否拦截 = false; 
+                if (!是否拦截 && Main.settings.和太吾之外的人都不能发生关系)
+                {
+                    是否拦截 = (fatherId == mainActorId || motherId == mainActorId);
+                    if(是否拦截)
+                        Debug("拦截发生关系，父方 " + DateFile.instance.GetActorName(fatherId) +
+                            "，母方 " + DateFile.instance.GetActorName(motherId));
+                }
+                if (!是否拦截 && Main.settings.太吾爱慕的人不能与他人发生关系)
+                {
+                    List<int> 太吾爱慕的人 = new List<int>();
+                    foreach(int k in Main.settings.爱慕关系.Keys)
+                    {
+                        if(Main.settings.爱慕关系[k])
+                            太吾爱慕的人.AddRange(DateFile.instance.GetActorSocial(mainActorId, k));
+                    }
+                    是否拦截 = (太吾爱慕的人.Contains(fatherId) || 太吾爱慕的人.Contains(motherId));
+                    if (是否拦截)
+                        Debug("拦截太吾爱慕的人发生关系，父方 " + DateFile.instance.GetActorName(fatherId) +
+                            "，母方 " + DateFile.instance.GetActorName(motherId));
+                }
+                if (!是否拦截 && Main.settings.爱慕太吾的人不能与他人发生关系)
+                {
+                    List<int> 这两人爱慕的人们 = new List<int>();
+                    foreach (int k in Main.settings.爱慕关系.Keys)
+                    {
+                        if (Main.settings.爱慕关系[k])
+                        {
+                            这两人爱慕的人们.AddRange(DateFile.instance.GetActorSocial(fatherId, k));
+                            这两人爱慕的人们.AddRange(DateFile.instance.GetActorSocial(motherId, k));
+                        }
+                    }
+                    是否拦截 = (这两人爱慕的人们.Contains(mainActorId));
+                    if (是否拦截)
+                        Debug("拦截爱慕太吾的人发生关系，父方 " + DateFile.instance.GetActorName(fatherId) +
+                            "，母方 " + DateFile.instance.GetActorName(motherId));
+                }
+                return !是否拦截;
+            }
+            else
+            {
+                // 不拦截
+                return true;
+            }
 		}
 	}
 }
--- DreamLover/PeopleLifeAI_UpdateTileCharsBehavior_Patch.cs
+++ DreamLover/PeopleLifeAI_UpdateTileCharsBehavior_Patch.cs
@@ -9,16 +9,24 @@ namespace DreamLover
 	[HarmonyPatch(typeof(PeopleLifeAI), "UpdateTileCharsBehavior")]
 	public static class PeopleLifeAI_UpdateTileCharsBehavior_Patch
 	{
+		private static void Debug(string str)
+		{
+			Main.Debug("<主动欺辱> " + str);
+		}
 		public static bool Prefix(int mapId, int tileId, bool isTaiwuAtThisTile, Dictionary<int, int> righteousInfo, object disasterInfo, int worldId, int mainActorId, Dictionary<int, List<int>> mainActorItems, System.Random random)
 		{
 			if (!Main.enabled || !Main.settings.启用主动欺辱功能)
 			{
 				return true;
 			}
+
 			if (!isTaiwuAtThisTile)
 			{
 				return true;
 			}
+
+			Debug("开始试图欺辱");
+
 			int 角色立场 = DateFile.instance.GetActorGoodness(mainActorId);
 			int 欺辱概率 = int.Parse(DateFile.instance.goodnessDate[角色立场][25]);
 			int 战力评价 = int.Parse(DateFile.instance.GetActorDate(mainActorId, 993, applyBonus: false));
@@ -39,17 +47,25 @@ namespace DreamLover
 				list = list.Where((int id) => int.Parse(DateFile.instance.GetActorDate(id, 14, applyBonus: false)) != 性别).ToList();
 			}
 
-			Debug.Log(("目标名单：" + ((list.Count == 0) ? "None" : (DateFile.instance.GetActorName(list[0]) + " etc."))));
+			string names = "";
+			foreach(int kid in list) 
+				names += DateFile.instance.GetActorName(list[kid]) + " ";
+
+			Debug("主角想要欺辱的目标名单" + ((list.Count == 0) ? "为空" : (": " + names)));
+
 			if (Main.settings.指定主动欺辱概率)
 			{
 				欺辱概率 = Main.settings.主动欺辱概率;
 			}
-			if (list.Count > 0 && UnityEngine.Random.Range(0, 100) < 欺辱概率)
+			int var1 = UnityEngine.Random.Range(0, 100);
+			if (list.Count > 0 && var1 < 欺辱概率)
 			{
+				Debug("欺辱概率判定通过，需求 " + var1 + "，结果 " + 欺辱概率);
 				int num3 = list[UnityEngine.Random.Range(0, list.Count)];
 				if (!Main.settings.跳过战力检定 && 战力评价 < int.Parse(DateFile.instance.GetActorDate(num3, 993, applyBonus: false)) + 10000)
 				{
-					Debug.Log("欺辱打不过的人");
+					Debug("战力判定未通过，需求 " + (int.Parse(DateFile.instance.GetActorDate(num3, 993, applyBonus: false)) + 10000) +
+						"，结果 " + 战力评价);
 					if (Main.settings.主动欺辱结仇)
 					{
 						DateFile.instance.AddSocial(num3, mainActorId, 401);
@@ -62,26 +78,28 @@ namespace DreamLover
 				}
 				else
 				{
+					Debug("战力判定通过，需求 " + (int.Parse(DateFile.instance.GetActorDate(num3, 993, applyBonus: false)) + 10000) +
+						"，结果 " + 战力评价);
 					if (Main.settings.主动欺辱影响双方情绪)
 					{
 						PeopleLifeAI_Utils.AiMoodChange(mainActorId, int.Parse(DateFile.instance.goodnessDate[DateFile.instance.GetActorGoodness(mainActorId)][102]) * 10);
 					}
 					if (DateFile.instance.GetActorSocial(num3, 312).Contains(mainActorId))
 					{
-						Debug.Log("欺辱成功，但是并无过多怨恨");
 						if (Main.settings.主动欺辱影响双方情绪)
 						{
 							PeopleLifeAI_Utils.AiMoodChange(num3, UnityEngine.Random.Range(-10, 11));
 						}
-						if (UnityEngine.Random.Range(0, 100) < 50 && Main.settings.主动欺辱结仇)
+						int var2 = UnityEngine.Random.Range(0, 100);
+						if (var2 < 50 && Main.settings.主动欺辱结仇)
 						{
 							DateFile.instance.AddSocial(num3, mainActorId, 402);
 						}
 						PeopleLifeAI_Utils.AISetMassage(97, num3, mapId, tileId, new int[1], mainActorId);
+						Debug("对方并无过多怨恨，" + ((var2 < 50) ? "但是仍然结下仇怨" : "因此并未结下仇怨"));
 					}
 					else
 					{
-						Debug.Log("欺辱成功，对方伤心欲绝");
 						if (Main.settings.主动欺辱影响双方情绪)
 						{
 							PeopleLifeAI_Utils.AiMoodChange(num3, -50);
@@ -91,20 +109,25 @@ namespace DreamLover
 							DateFile.instance.AddSocial(num3, mainActorId, 402);
 						}
 						PeopleLifeAI_Utils.AISetMassage(96, num3, mapId, tileId, new int[1], mainActorId);
+						Debug("对方伤心欲绝");
 					}
 					if (性别 != int.Parse(DateFile.instance.GetActorDate(num3, 14, applyBonus: false)))
 					{
-						Debug.Log("试图怀孕");
+						Debug("由于性别不同，开始进行怀孕判定");
 						PeopleLifeAI_Utils.AISetChildren((性别 == 1) ? mainActorId : num3, (性别 == 1) ? num3 : mainActorId, Main.settings.主动欺辱的孩子有双亲 ? 1 : ((性别 != 1) ? 1 : 0), Main.settings.主动欺辱的孩子有双亲 ? 1 : ((性别 == 1) ? 1 : 0));
 					}
 					else
 					{
-						Debug.Log("同性不能怀孕");
+						Debug("由于性别相同，无法进行怀孕判定");
 						DateFile.instance.ChangeActorFeature(mainActorId, 4001, 4002);
 						DateFile.instance.ChangeActorFeature(num3, 4001, 4002);
 						GEvent.OnEvent(eEvents.Copulate, mainActorId, num3);
 					}
 				}
+			}
+			else
+			{
+				Debug("欺辱概率判定未通过，需求 " + var1 + "，结果 " + 欺辱概率);
 			}
 			return true;
 		}
